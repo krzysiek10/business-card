3ba72d881ca48fc6a99577ab7e2923e27e8ce557
Krzysztof Rakoczy <krzysztof.rakoczy@salesbook.com>
2025-12-03 13:27:07 +0100

    modal form

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('contactModal');
     const contactButtons = document.querySelectorAll('a.btn');
     const submitButton = document.querySelector('.modal__form button');
+    const modalSuccessContent = document.querySelector('.modal__success-content');
+    const modalFormContent = document.querySelector('.modal__form-content');
+    const closeSuccessButton = document.querySelector('.js-close-success');
 
     contactButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             e.preventDefault();
             modal.classList.add('active');
+
+            modalFormContent.style.display = 'block';
+            modalSuccessContent.style.display = 'none';
         });
     });
 
@@ -21,6 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
         submitButton.addEventListener('click', (e) => {
             e.preventDefault();
             console.log('Form submitted');
+            
+            modalFormContent.style.display = 'none';
+            modalSuccessContent.style.display = 'block';
+        });
+    }
+
+    if (closeSuccessButton) {
+        closeSuccessButton.addEventListener('click', (e) => {
+            e.preventDefault();
             modal.classList.remove('active');
         });
     }
